af2579963b5d7989ff948a4a30f6b9ab35a2d7c7
Jobson Almeida <jobson.ra@outlook.com>
2025-02-03 15:31:23 -0300

    chore: inicializar GPIO do botão e definí-lo como entrada pull up

--- timer_02.c
+++ timer_02.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define BUTTON_A 5
+
 int main()
 {
     stdio_init_all();
@@ -8,6 +10,11 @@ int main()
     // incializa as GPIOs dos LEDs por meio de uma máscara - 0b011100000000000
     gpio_init_mask(0x07 << 11);
     gpio_set_dir_out_masked(0x07 << 11); // define as GPIOs dos LEDs como saídas, utilizando uma máscara
+
+    // inicializa e configura o botão como entrada e resistor pull-up interno.
+    gpio_init(BUTTON_A);
+    gpio_set_dir(BUTTON_A, GPIO_IN);
+    gpio_pull_up(BUTTON_A);
 
     while (true)
     {
