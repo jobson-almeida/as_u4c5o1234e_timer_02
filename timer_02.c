15e64059cfaf4f005321be20cac7c50258e6c2de
Jobson Almeida <jobson.ra@outlook.com>
2025-02-03 15:29:18 -0300

    chore: inicializar GPIOs dos LEDs e definí-las como saídas

--- timer_02.c
+++ timer_02.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-
-
 int main()
 {
     stdio_init_all();
 
-    while (true) {
+    // incializa as GPIOs dos LEDs por meio de uma máscara - 0b011100000000000
+    gpio_init_mask(0x07 << 11);
+    gpio_set_dir_out_masked(0x07 << 11); // define as GPIOs dos LEDs como saídas, utilizando uma máscara
+
+    while (true)
+    {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
